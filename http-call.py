3c228dac00052bb327fdaef0c115432ae764688c
halkaab3 <halkaab3@asu.edu>
2023-01-06 18:47:14 -0700

    editing http file

--- http-call.py
+++ http-call.py
@@ -5,7 +5,7 @@ for url in ['https://github.com', 'https://api.github.com/invalid']:
     try:
         response = requests.get(url)
 
-        # If the response was successful, no Exception will be raised
+        # If the you get response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')  # Python 3.6
